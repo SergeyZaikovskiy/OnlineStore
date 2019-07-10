1febf29cc4ab0552edaeff70b008c95ed8b224ea
sega6300 <sega6300@gmail.com>
2019-07-10 15:12:43 +0500

    add cart controller

--- OnlineStore/Controllers/CartController.cs
+++ OnlineStore/Controllers/CartController.cs
@@ -3,14 +3,84 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Infrastructure.Interfeices;
+using OnlineStore.ViewModels;
 
 namespace OnlineStore.Controllers
 {
+    /// <summary>
+    /// Контроллер работы с корзиной
+    /// </summary>
     public class CartController : Controller
     {
-        public IActionResult Index()
+        private readonly ICartService cartService;
+        private readonly IOrderService orderService;
+
+        public CartController(ICartService cartService, IOrderService orderService)
         {
+            this.cartService = cartService;
+            this.orderService = orderService;
+        }
+
+        public IActionResult Details()
+        {
+            var model = new CartDetailsViewModel
+            {
+                CartViewModel = cartService.TransformCart(),
+                OrderViewModel = new OrderViewModel()
+            };
+
+            return View(model);
+        }
+
+        public IActionResult DecrementFromCart(int id)
+        {
+            cartService.DecrementCountItem(id);
+            return RedirectToAction("Details");
+        }
+
+        public IActionResult RemoveFromCart(int id)
+        {
+            cartService.RemoveFromCart(id);
+            return RedirectToAction("Details");
+        }
+
+        public IActionResult RemoveAll()
+        {
+            cartService.RemoveAll();
+            return RedirectToAction("Details");
+        }
+
+        public IActionResult AddtoCart(int id)
+        {
+            cartService.AddToCart(id);
+            return RedirectToAction("Details");
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult CheckOut(OrderViewModel orderViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(nameof(Details), new CartDetailsViewModel
+                {
+                    CartViewModel = cartService.TransformCart(),
+                    OrderViewModel = orderViewModel
+                });
+
+            var order = orderService.CreateNewOrder(orderViewModel, cartService.TransformCart(), User.Identity.Name);
+
+
+            cartService.RemoveAll();
+
+            return RedirectToAction("OrderConfirmed", new { id = order.id });
+        }
+
+        public IActionResult OrderConfirmed(int id)
+        {
+            ViewBag.OrderId = id;
             return View();
         }
+
     }
+
 }
--- OnlineStore/Controllers/ProfileController.cs
+++ OnlineStore/Controllers/ProfileController.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Infrastructure.Interfeices;
+using OnlineStore.ViewModels;
 
 namespace OnlineStore.Controllers
 {
     public class ProfileController : Controller
     {
-        public IActionResult Index()
+        private readonly IOrderService orderService;
+
+        public ProfileController(IOrderService orderService)
         {
-            return View();
+            this.orderService = orderService;
+        }
+
+        public IActionResult Index() => View();
+
+        public IActionResult Orders()
+        {
+            var orders = orderService.GetUserOrders(User.Identity.Name);
+
+            return View(orders.Select(order => new UserOrderViewModel
+            {
+                Id = order.id,
+                Name = order.Name,
+                Phone = order.Phone,
+                Address = order.Address,
+                TotalSum = order.OrderItems.Sum(o => o.Quantity * o.Price)
+            }));
         }
     }
+
 }
