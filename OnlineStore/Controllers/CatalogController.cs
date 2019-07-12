1943b22876f2baed2c3cea2bd279fde04e35f8ac
sega6300 <sega6300@gmail.com>
2019-07-12 15:40:51 +0500

    changed header navbar (add sections)

--- OnlineStore/Controllers/CatalogController.cs
+++ OnlineStore/Controllers/CatalogController.cs
@@ -12,7 +12,8 @@ namespace OnlineStore.Controllers
     public class CatalogController : Controller
     {
         public IActionResult Index()
-        {
+        {        
+            
             return View();
         }
     }
